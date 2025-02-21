cd306927004a413cf29c75c8d4614b8dd6b69556
abnody <ahmedalabnody45@gmail.com>
2025-02-21 04:47:09 +0200

    Updates

--- assets/NotFound-BgEYwKaH.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-B4CT-cHI.js";const e="/Ecommerce/assets/404-BeEWpfLg.gif";function t(){return s.jsx(s.Fragment,{children:s.jsx("div",{className:" py-5",children:s.jsx("img",{src:e,alt:"",className:" mx-auto w-96"})})})}export{t as default};
